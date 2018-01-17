00c8c88be94636ea1b6e3087aca908941269396d
David Kopal <kopaldvd@gmail.com>
2018-01-17 22:17:13 +0100

    create itemQuantitySaga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,3 +1,4 @@
 export { currentUserSaga } from "./currentUserSaga"
 export { fetchCartSaga } from "./fetchCartSaga"
 export { itemsDetailsSaga } from "./itemsDetailsSaga"
+export { itemQuantitySaga } from "./itemQuantitySaga"
--- src/utility/makeActionCreator.js
+++ src/utility/makeActionCreator.js
@@ -1,7 +1,6 @@
 export const makeActionCreator = (type, ...argNames) => {
     return function(...args) {
         let action = { type };
-        console.log("ARGS", args)
         argNames.forEach((arg, index) => {
             action[argNames[index]] = args[index]
         });
