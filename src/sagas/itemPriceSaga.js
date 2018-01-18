3f0977fd50399a4b5a1d3689b9364c2a3320f813
David Kopal <kopaldvd@gmail.com>
2018-01-18 13:27:51 +0100

    create item price saga

---
+++ src/sagas/itemPriceSaga.js
@@ -0,0 +1,25 @@
+import { all, take, select, put, call, apply, fork } from 'redux-saga/effects'
+import {
+  SET_CART_ITEMS,
+  SET_CURRENT_USER,
+  SET_ITEM_DETAILS,
+  setItemPrice
+} from './../actions'
+import fetch from 'isomorphic-fetch'
+
+export function * fetchItemPrice(itemId, currency){
+  const response = yield call(
+    fetch,
+    `http://localhost:8081/prices/${currency}/${itemId}`
+  )
+  const itemDetails = yield apply(response, response.json)
+  yield put(setItemPrice(itemId, itemDetails[0].price))
+}
+
+export function* itemPriceSaga() {
+  const [{ user }, { items }] = yield all([
+    take(SET_CURRENT_USER),
+    take(SET_CART_ITEMS)
+  ])
+  yield items.map(item=> fork(fetchItemPrice, item.id, user.country))
+}
